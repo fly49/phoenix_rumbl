14c694a46c93b2c955a4646086fa0c8af83c0b95
fly49 <flymaipie@gmail.com>
2021-02-09 18:44:37 +0300

    handle disconnects

--- lib/rumbl_web/channels/video_channel.ex
+++ lib/rumbl_web/channels/video_channel.ex
@@ -3,13 +3,14 @@ defmodule RumblWeb.VideoChannel do
   alias RumblWeb.AnnotationView
   alias Rumbl.{Accounts, Multimedia}
 
-  def join("videos:" <> video_id, _params, socket) do
+  def join("videos:" <> video_id, params, socket) do
+    last_seen_id = params["last_seen_id"] || 0
     video_id = String.to_integer(video_id)
     video = Multimedia.get_video!(video_id)
 
     annotations =
       video
-      |> Multimedia.list_anntotations()
+      |> Multimedia.list_anntotations(last_seen_id)
       |> Phoenix.View.render_many(AnnotationView, "annotation.json")
 
     {:ok, %{annotations: annotations}, assign(socket, :video_id, video_id)}
